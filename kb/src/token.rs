0b7d849c9a44a2da5fb555bb262047023c37e88e
Efe Saatci <efesaatci2019@u.northwestern.edu>
2019-06-02 21:49:09 -0500

    tokenizing a text file

---
+++ kb/src/token.rs
@@ -0,0 +1,31 @@
+fn tokenize_file(filename: &str) -> std::io::Result<()> {
+
+	let file = File::open("fact.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    // pattern to to detect facts
+    let re = Regex::new(r"fact:\s\((.+)\)\n").unwrap();
+    let facts: Vec<Fact> = Vec::new();
+    let rules: Vec<Fact> = Vec::new();
+    
+    for content in re.captures_iter(&contents) {
+    	println!("{:?}", &content[1]);
+	}
+    Ok(())
+}
+
+
+
+// Structure to hold parsed facts and rules
+#[derive(Debug)]
+struct ParsedTokens {
+	facts: Vec<Fact>,
+	rules: Vec<Rule>,
+}
+
+impl ParsedTokens {
+	pub fn new(facts: Vec<Fact>, rules: Vec<Rule>) -> Self {
+		ParsedTokens{facts: facts, rules: rules}
+	}
+}  
