899bb1cdd3d3bf2e3a5f50b2220aa0630a7f68ab
Efe Saatci <efesaatci2019@u.northwestern.edu>
2019-06-06 01:57:28 -0500

    Progress

--- kb/src/main.rs
+++ kb/src/main.rs
@@ -6,6 +6,9 @@ use crate::fact::Fact;
 use crate::rule::Rule;
 use crate::symbols::Symbol;
 use std::rc::Rc;
+use crate::token::tokenize_file;
+use std::process;
+use std::env;
 mod fact;
 mod kb;
 mod rule;
@@ -14,6 +17,35 @@ mod symbols;
 mod token;
 
 fn main() {
-    println!("Hello, world!");
-    //let my_kb = kb::KnowledgeBase::new();
+    
+	// get the command line arguments 
+	// parse the command line arguments 
+	// try to retrieve the filename
+	// pass the filename to the tokenize_file
+
+	let args: Vec<String> = env::args().collect();
+	
+	if args.len() != 2 {
+		eprintln!("not enough arguments");
+		process::exit(1);
+	}
+
+    let mut my_kb = kb::KnowledgeBase::new();    
+    let parsed_tokens = tokenize_file(&args[1]);
+    
+
+    match parsed_tokens {
+    	Ok(rules_and_facts) => {
+    		for rf in rules_and_facts {
+    			my_kb.assert(rf);
+    		}
+    	},
+    	Err(e) => {
+    		eprintln!("{}", e);
+    		process::exit(1);
+    	},
+    }
+
+    // loop 
+    // handle_command_line()
 }
